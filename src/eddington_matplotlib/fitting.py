b64966d6530b0f73c5887d027ddacd9f1fd94a7b
Sagi Shadur <saroad2@gmail.com>
2020-06-01 16:14:25 +0300

    Make intentional pylint error
    This is done in order to check CI/CD pipeline

--- src/eddington_matplotlib/fitting.py
+++ src/eddington_matplotlib/fitting.py
@@ -12,7 +12,7 @@ from eddington_matplotlib.util import (
 )
 
 
-def plot_fitting(  # pylint: disable=C0103,R0913
+def plot_fitting(
     func,
     data: FitData,
     plot_configuration: PlotConfiguration,
