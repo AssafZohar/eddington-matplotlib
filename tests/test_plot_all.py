893dbbb023f3505ba56ac248ea7fbdff7fd4b81e
Sagi Shadur <saroad2@gmail.com>
2020-06-18 17:14:39 +0300

    Make test_plot_all.py use pytest

--- tests/test_plot_all.py
+++ tests/test_plot_all.py
@@ -1,119 +1,128 @@
+import pytest
 from pathlib import Path
-from typing import Dict
 
-from eddington_test import MetaTestCase
-from mock import patch, Mock
+from eddington_core import FitData
+from mock import Mock
 import numpy as np
 
 from eddington_matplotlib import PlotConfiguration, OutputConfiguration, plot_all
 from tests.base_test_cases import dummy_func
 
 
-class PlotAllMetaTestCase(MetaTestCase):
-    should_print_results = True
-    should_plot_fitting = True
-    should_plot_residuals = True
-    should_plot_data = False
+should_print_results = True
+should_plot_fitting = True
+should_plot_residuals = True
+should_plot_data = False
 
-    data = "data"
-    xmin = 0.2
-    xmax = 9.8
-    func = dummy_func
-    output_dir = Path("dir/to/output")
+xmin = 0.2
+xmax = 9.8
+func = dummy_func
+data = FitData.random(dummy_func)
+a = np.array([1, 2])
+output_dir = Path("dir/to/output")
 
-    def setUp(self):
-        self.result = Mock()
-        self.result.a = np.array([1, 2])
 
-        plot_data_patcher = patch("eddington_matplotlib.all.plot_data")
-        plot_fitting_patcher = patch("eddington_matplotlib.all.plot_fitting")
-        plot_residuals_patcher = patch("eddington_matplotlib.all.plot_residuals")
+@pytest.fixture
+def plot_functions_mock(mocker):
 
-        self.plot_data = plot_data_patcher.start()
-        self.plot_fitting = plot_fitting_patcher.start()
-        self.plot_residuals = plot_residuals_patcher.start()
+    plot_data = mocker.patch("eddington_matplotlib.all.plot_data")
+    plot_fitting = mocker.patch("eddington_matplotlib.all.plot_fitting")
+    plot_residuals = mocker.patch("eddington_matplotlib.all.plot_residuals")
+    result = Mock()
+    result.a = a
+    return dict(
+        plot_data=plot_data,
+        plot_fitting=plot_fitting,
+        plot_residuals=plot_residuals,
+        result=result,
+    )
 
-        self.addCleanup(plot_data_patcher.stop)
-        self.addCleanup(plot_fitting_patcher.stop)
-        self.addCleanup(plot_residuals_patcher.stop)
 
-        self.plot_configuration = PlotConfiguration.build(
-            base_name=self.func.name, xmin=self.xmin, xmax=self.xmax, **self.kwargs
+@pytest.fixture(
+    params=[
+        dict(),
+        dict(plot_data=True),
+        dict(plot_fitting=False),
+        dict(plot_residuals=False),
+        dict(print_results=False),
+    ]
+)
+def case_data(request, plot_functions_mock):
+    kwargs = request.param
+    plot_configuration = PlotConfiguration.build(
+        base_name=func.name, xmin=xmin, xmax=xmax, **kwargs
+    )
+    output_configuration = OutputConfiguration.build(
+        base_name=func.name, output_dir=output_dir
+    )
+
+    plot_all(
+        func=func,
+        data=data,
+        plot_configuration=plot_configuration,
+        output_configuration=output_configuration,
+        result=plot_functions_mock["result"],
+    )
+    inp = dict(
+        plot_configuration=plot_configuration,
+        output_configuration=output_configuration,
+        kwargs=kwargs,
+    )
+    return inp, plot_functions_mock
+
+
+def test_export_result(case_data):
+    inp, mocks = case_data
+    kwargs = inp["kwargs"]
+    result = mocks["result"]
+    if kwargs.get("print_results", True):
+        result.print_or_export.assert_called_once_with(
+            inp["output_configuration"].result_output_path,
         )
-        self.output_configuration = OutputConfiguration.build(
-            base_name=self.func.name, output_dir=self.output_dir
+    else:
+        result.print_or_export.assert_not_called()
+
+
+def test_plot_data(case_data):
+    inp, mocks = case_data
+    kwargs = inp["kwargs"]
+    if kwargs.get("plot_data", False):
+        mocks["plot_data"].assert_called_once_with(
+            data=data,
+            plot_configuration=inp["plot_configuration"],
+            output_path=inp["output_configuration"].data_output_path,
         )
+    else:
+        mocks["plot_data"].assert_not_called()
 
-        plot_all(
-            func=self.func,
-            data=self.data,
-            plot_configuration=self.plot_configuration,
-            output_configuration=self.output_configuration,
-            result=self.result,
+
+def test_plot_fitting(case_data):
+    inp, mocks = case_data
+    kwargs = inp["kwargs"]
+    result = mocks["result"]
+    if kwargs.get("plot_fitting", True):
+        mocks["plot_fitting"].assert_called_once_with(
+            func=func,
+            data=data,
+            a=result.a,
+            plot_configuration=inp["plot_configuration"],
+            output_path=inp["output_configuration"].fitting_output_path,
         )
-
-    def test_export_result(self):
-        if self.should_print_results:
-            self.result.print_or_export.assert_called_once_with(
-                self.output_configuration.result_output_path,
-            )
-        else:
-            self.result.export_to_file.assert_not_called()
-
-    def test_plot_data(self):
-        if self.should_plot_data:
-            self.plot_data.assert_called_once_with(
-                data=self.data,
-                plot_configuration=self.plot_configuration,
-                output_path=self.output_configuration.data_output_path,
-            )
-        else:
-            self.plot_data.assert_not_called()
-
-    def test_plot_fitting(self):
-        if self.should_plot_fitting:
-            self.plot_fitting.assert_called_once_with(
-                func=self.func,
-                data=self.data,
-                a=self.result.a,
-                plot_configuration=self.plot_configuration,
-                output_path=self.output_configuration.fitting_output_path,
-            )
-        else:
-            self.plot_fitting.assert_not_called()
-
-    def test_plot_residuals(self):
-        if self.should_plot_residuals:
-            self.plot_residuals.assert_called_once_with(
-                func=self.func,
-                data=self.data,
-                a=self.result.a,
-                plot_configuration=self.plot_configuration,
-                output_path=self.output_configuration.residuals_output_path,
-            )
-        else:
-            self.plot_residuals.assert_not_called()
+    else:
+        mocks["plot_fitting"].assert_not_called()
 
 
-class TestPlotAllDefault(metaclass=PlotAllMetaTestCase):
-    kwargs: Dict = dict()
-
-
-class TestPlotWithPlotData(metaclass=PlotAllMetaTestCase):
-    kwargs = dict(plot_data=True)
-    should_plot_data = True
-
-
-class TestPlotWithoutPlotFitting(metaclass=PlotAllMetaTestCase):
-    kwargs = dict(plot_fitting=False)
-    should_plot_fitting = False
-
-
-class TestPlotWithoutPlotResiduals(metaclass=PlotAllMetaTestCase):
-    kwargs = dict(plot_residuals=False)
-    should_plot_residuals = False
-
-
-class TestPlotWithoutExportResult(metaclass=PlotAllMetaTestCase):
-    kwargs = dict(print_results=False)
-    should_print_results = False
+def test_plot_residuals(case_data):
+    inp, mocks = case_data
+    kwargs = inp["kwargs"]
+    result = mocks["result"]
+    if kwargs.get("plot_residuals", True):
+        mocks["plot_residuals"].assert_called_once_with(
+            func=func,
+            data=data,
+            a=result.a,
+            plot_configuration=inp["plot_configuration"],
+            output_path=inp["output_configuration"].residuals_output_path,
+        )
+    else:
+        mocks["plot_residuals"].assert_not_called()
