e3ca3c04dcfe6a24cb48a9e3d4e15e372c91b1c4
Sagi Shadur <saroad2@gmail.com>
2020-06-18 17:46:32 +0300

    Make test_plot_configuration.py use pytest

--- tests/test_plot_configuration.py
+++ tests/test_plot_configuration.py
@@ -1,174 +1,149 @@
+import pytest
+from pytest_cases import cases_data, THIS_MODULE
 from typing import Union
-from unittest import TestCase
 
 from eddington_matplotlib import PlotConfiguration
 
 
-class PlotConfigurationBaseTestCase:
-    decimal = 5
+delta = 1e-5
 
-    xmin = 0.2
-    xmax = 9.8
-    base_name = "Base Name"
-    grid = False
-    xcolumn: Union[str, float, None] = "x_column_example"
-    xlabel: Union[str, float, None] = xcolumn
-    ycolumn: Union[str, float, None] = "y_column_example"
-    ylabel: Union[str, float, None] = ycolumn
-    title = "Base Name Fitting"
-    residuals_title = "Base Name Fitting - Residuals"
+xmin = 0.2
+xmax = 9.8
+base_name = "Base Name"
+grid = False
+xcolumn: Union[str, float, None] = "x_column_example"
+ycolumn: Union[str, float, None] = "y_column_example"
+default_title = "Base Name Fitting"
+default_data_title = "Base Name Fitting - Data"
+default_residuals_title = "Base Name Fitting - Residuals"
 
-    @classmethod
-    def build_kwargs(
-        cls,
-        xlabel=None,
-        ylabel=None,
-        title=None,
-        residuals_title=None,
-        data_title=None,
-        grid=False,
-    ):
-        kwargs = dict(
-            xlabel=xlabel,
-            ylabel=ylabel,
-            title=title,
-            residuals_title=residuals_title,
-            data_title=data_title,
-            grid=grid,
-        )
-        return kwargs
-
-    def setUp(self):
-        self.plot_configuration = PlotConfiguration.build(
-            base_name=self.base_name,
-            xmin=self.xmin,
-            xmax=self.xmax,
-            xcolumn=self.xcolumn,
-            ycolumn=self.ycolumn,
-            **self.kwargs
-        )
-
-    def test_xmin(self):
-        self.assertAlmostEqual(
-            self.xmin,
-            self.plot_configuration.xmin,
-            places=self.decimal,
-            msg="X min value is different than expected",
-        )
-
-    def test_xmax(self):
-        self.assertAlmostEqual(
-            self.xmax,
-            self.plot_configuration.xmax,
-            places=self.decimal,
-            msg="X max value is different than expected",
-        )
-
-    def test_xlabel(self):
-        self.assertEqual(
-            self.xlabel,
-            self.plot_configuration.xlabel,
-            msg="X label is different than expected",
-        )
-
-    def test_ylabel(self):
-        self.assertEqual(
-            self.ylabel,
-            self.plot_configuration.ylabel,
-            msg="Y label is different than expected",
-        )
-
-    def test_title(self):
-        self.assertEqual(
-            self.title,
-            self.plot_configuration.title,
-            msg="Title is different than expected",
-        )
-
-    def test_residuals_title(self):
-        self.assertEqual(
-            self.residuals_title,
-            self.plot_configuration.residuals_title,
-            msg="Residuals title is different than expected",
-        )
-
-    def test_grid(self):
-        if self.grid:
-            self.assertTrue(self.plot_configuration.grid, msg="Grid should be true")
-        else:
-            self.assertFalse(self.plot_configuration.grid, msg="Grid should be false")
+common_kwargs = dict(
+    base_name=base_name, xmin=xmin, xmax=xmax, xcolumn=xcolumn, ycolumn=ycolumn,
+)
 
 
-class TestPlotConfigurationWithStringHeaders(TestCase, PlotConfigurationBaseTestCase):
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs()
-
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+def case_no_kwargs():
+    return PlotConfiguration.build(**common_kwargs), dict()
 
 
-class TestPlotConfigurationWithXLabelOverride(TestCase, PlotConfigurationBaseTestCase):
-    xlabel = "some x label"
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(xlabel=xlabel)
-
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+def case_x_label_override():
+    kwargs = dict(xlabel="some x label")
+    return PlotConfiguration.build(**common_kwargs, **kwargs), kwargs
 
 
-class TestPlotConfigurationWithYLabel(TestCase, PlotConfigurationBaseTestCase):
-    ylabel = "some y label"
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(ylabel=ylabel)
-
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+def case_y_label_override():
+    kwargs = dict(ylabel="some y label")
+    return PlotConfiguration.build(**common_kwargs, **kwargs), kwargs
 
 
-class TestPlotConfigurationWithXAndYLabel(TestCase, PlotConfigurationBaseTestCase):
-    xlabel = "xlabel"
-    ylabel = "ylabel"
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(xlabel=xlabel, ylabel=ylabel)
-
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+def case_both_x_and_y_label_override():
+    kwargs = dict(xlabel="some x label", ylabel="some y label")
+    return PlotConfiguration.build(**common_kwargs, **kwargs), kwargs
 
 
-class TestPlotConfigurationWithNumberHeaders(TestCase, PlotConfigurationBaseTestCase):
-    xcolumn, ycolumn = 5.2, 2
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs()
-
-    xlabel = ylabel = None
-
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+def case_both_x_and_y_wth_number_headers():
+    kwargs = dict(xcolumn=5.2, ycolumn=2)
+    return (
+        PlotConfiguration.build(base_name=base_name, xmin=xmin, xmax=xmax, **kwargs),
+        dict(xlabel=None, ylabel=None),
+    )
 
 
-class TestPlotConfigurationWithTitle(TestCase, PlotConfigurationBaseTestCase):
+def case_title():
     title = "An Awesome Title"
     residuals_title = "An Awesome Title - Residuals"
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(title=title)
+    data_title = "An Awesome Title - Data"
+    kwargs = dict(title=title)
+    return (
+        PlotConfiguration.build(**common_kwargs, **kwargs),
+        dict(title=title, residuals_title=residuals_title, data_title=data_title),
+    )
 
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
 
-
-class TestPlotConfigurationWithResidualsTitle(TestCase, PlotConfigurationBaseTestCase):
+def case_residuals_title():
     residuals_title = "An Awesome Residuals Title"
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(residuals_title=residuals_title)
+    kwargs = dict(residuals_title=residuals_title)
+    return (
+        PlotConfiguration.build(**common_kwargs, **kwargs),
+        dict(residuals_title=residuals_title),
+    )
 
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
 
-
-class TestPlotConfigurationWithDataTitle(TestCase, PlotConfigurationBaseTestCase):
+def case_data_title():
     data_title = "An Awesome Data Title"
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(data_title=data_title)
+    kwargs = dict(data_title=data_title)
+    return (
+        PlotConfiguration.build(**common_kwargs, **kwargs),
+        dict(data_title=data_title),
+    )
 
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+
+def case_grid():
+    return (
+        PlotConfiguration.build(**common_kwargs, grid=True),
+        dict(grid=True),
+    )
 
 
-class TestPlotConfigurationWithGrid(TestCase, PlotConfigurationBaseTestCase):
-    grid = True
-    kwargs = PlotConfigurationBaseTestCase.build_kwargs(grid=True)
+@cases_data(module=THIS_MODULE)
+def test_xmin(case_data):
+    plot_configuration, _ = case_data.get()
+    assert xmin == pytest.approx(
+        plot_configuration.xmin, rel=delta
+    ), "X min value is different than expected"
 
-    def setUp(self):
-        PlotConfigurationBaseTestCase.setUp(self)
+
+@cases_data(module=THIS_MODULE)
+def test_xmax(case_data):
+    plot_configuration, _ = case_data.get()
+    assert xmax == pytest.approx(
+        plot_configuration.xmax, rel=delta
+    ), "X max value is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_xlabel(case_data):
+    plot_configuration, expected = case_data.get()
+    xlabel = expected.get("xlabel", xcolumn)
+    assert plot_configuration.xlabel == xlabel, "X label is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_ylabel(case_data):
+    plot_configuration, expected = case_data.get()
+    ylabel = expected.get("ylabel", ycolumn)
+    assert plot_configuration.ylabel == ylabel, "Y label is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_title(case_data):
+    plot_configuration, expected = case_data.get()
+    title = expected.get("title", default_title)
+    assert plot_configuration.title == title, "Title is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_residuals_title(case_data):
+    plot_configuration, expected = case_data.get()
+    residuals_title = expected.get("residuals_title", default_residuals_title)
+    assert (
+        plot_configuration.residuals_title == residuals_title
+    ), "Residuals title is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_data_title(case_data):
+    plot_configuration, expected = case_data.get()
+    data_title = expected.get("data_title", default_data_title)
+    assert (
+        plot_configuration.data_title == data_title
+    ), "Data title is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_grid(case_data):
+    plot_configuration, expected = case_data.get()
+    if expected.get("grid", False):
+        assert plot_configuration.grid, "Grid should be true"
+    else:
+        assert not plot_configuration.grid, "Grid should be false"
