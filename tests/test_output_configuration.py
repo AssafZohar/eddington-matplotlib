ae7eea13a4883a6a04eb88c2f4f3205198b23f4a
Sagi Shadur <saroad2@gmail.com>
2020-06-18 16:39:46 +0300

    Make test_output_configuration.py use pytest

--- tests/test_output_configuration.py
+++ tests/test_output_configuration.py
@@ -1,59 +1,64 @@
+from pytest_cases import cases_data, THIS_MODULE
 from pathlib import Path
 
 from eddington_matplotlib import OutputConfiguration
-from eddington_test import MetaTestCase
 
 
-class OutputConfigurationBaseTestCase(MetaTestCase):
-    func_name = "func_name"
-    result_output_path = None
-    data_output_path = None
-    fitting_output_path = None
-    residuals_output_path = None
-
-    def setUp(self):
-        self.output_configuration = OutputConfiguration.build(
-            base_name=self.func_name, output_dir=self.output_dir
-        )
-
-    def test_result_output_path(self):
-        self.assertEqual(
-            self.result_output_path,
-            self.output_configuration.result_output_path,
-            msg="Data output path is different than expected",
-        )
-
-    def test_data_output_path(self):
-        self.assertEqual(
-            self.data_output_path,
-            self.output_configuration.data_output_path,
-            msg="Data output path is different than expected",
-        )
-
-    def test_fitting_output_path(self):
-        self.assertEqual(
-            self.fitting_output_path,
-            self.output_configuration.fitting_output_path,
-            msg="Fitting output path is different than expected",
-        )
-
-    def test_residuals_output_path(self):
-        self.assertEqual(
-            self.residuals_output_path,
-            self.output_configuration.residuals_output_path,
-            msg="Residuals output path is different than expected",
-        )
+func_name = "func_name"
 
 
-class TestOutputConfigurationWithoutOutputDir(
-    metaclass=OutputConfigurationBaseTestCase
-):
-    output_dir = None
+def case_no_output_dir():
+    output_configuration = OutputConfiguration.build(base_name=func_name)
+    expected = dict(
+        result_output_path=None,
+        data_output_path=None,
+        fitting_output_path=None,
+        residuals_output_path=None,
+    )
+    return output_configuration, expected
 
 
-class TestOutputConfigurationWithOutputDir(metaclass=OutputConfigurationBaseTestCase):
+def case_with_output_fir():
     output_dir = Path("directory")
-    result_output_path = output_dir / "func_name_fitting_result.txt"
-    data_output_path = output_dir / "func_name_data.png"
-    fitting_output_path = output_dir / "func_name_fitting.png"
-    residuals_output_path = output_dir / "func_name_fitting_residuals.png"
+    output_configuration = OutputConfiguration.build(
+        base_name=func_name, output_dir=output_dir
+    )
+    expected = dict(
+        result_output_path=output_dir / "func_name_fitting_result.txt",
+        data_output_path=output_dir / "func_name_data.png",
+        fitting_output_path=output_dir / "func_name_fitting.png",
+        residuals_output_path=output_dir / "func_name_fitting_residuals.png",
+    )
+    return output_configuration, expected
+
+
+@cases_data(module=THIS_MODULE)
+def test_result_output_path(case_data):
+    output_configuration, expected = case_data.get()
+    assert (
+        expected["result_output_path"] == output_configuration.result_output_path
+    ), "Data output path is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_data_output_path(case_data):
+    output_configuration, expected = case_data.get()
+    assert (
+        expected["data_output_path"] == output_configuration.data_output_path
+    ), "Data output path is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_fitting_output_path(case_data):
+    output_configuration, expected = case_data.get()
+    assert (
+        expected["fitting_output_path"] == output_configuration.fitting_output_path
+    ), "Fitting output path is different than expected"
+
+
+@cases_data(module=THIS_MODULE)
+def test_residuals_output_path(case_data):
+    output_configuration, expected = case_data.get()
+    assert (
+        expected["residuals_output_path"] == output_configuration.residuals_output_path
+    ), "Residuals output path is different than expected"
