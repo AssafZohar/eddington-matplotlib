9e1a1bc82c73401c77fbd7a483bb21ef7f32ac85
Sagi Shadur <saroad2@gmail.com>
2020-06-19 14:16:03 +0300

    Fix plot_residuals tests

--- tests/plot/test_plot_residuals.py
+++ tests/plot/test_plot_residuals.py
@@ -14,71 +14,61 @@ from tests.plot import (
     xmin,
     xmax,
 )
-import tests.plot.plot_cases as cases
 
 
 @fixture_plus
-@cases_data(module=cases)
-def plot_residuals_fixture(case_data, plt_mock):
-    kwargs, output_path = case_data.get()
-    plot_configuration = PlotConfiguration(**kwargs)
-    plot_residuals(
-        func=dummy_func,
-        data=data,
-        a=a,
-        plot_configuration=plot_configuration,
-        output_path=output_path,
+def plot_residuals_fixture(configurations, plt_mock):
+    plot_configuration, output_configuration = configurations
+    fig = plot_residuals(
+        a=a, func=dummy_func, data=data, plot_configuration=plot_configuration,
     )
-    return plt_mock, dict(output_path=output_path, **kwargs)
+    return fig, plot_configuration, plt_mock
+
+
+def test_result_figure(plot_residuals_fixture):
+    figure, plot_configuration, mocks = plot_residuals_fixture
+    assert figure == mocks["figure"], "Returned figure is different than expected"
 
 
 def test_title(plot_residuals_fixture):
-    mocks, expected = plot_residuals_fixture
-    plt, figure = mocks["plt"], mocks["figure"]
-    check_title(plt=plt, figure=figure, title=expected.get("residuals_title", None))
+    figure, plot_configuration, mocks = plot_residuals_fixture
+    plt = mocks["plt"]
+    check_title(plt=plt, figure=figure, title=plot_configuration.residuals_title)
 
 
 def test_xlabel(plot_residuals_fixture):
-    mocks, expected = plot_residuals_fixture
-    plt, figure = mocks["plt"], mocks["figure"]
-    check_x_label(plt=plt, figure=figure, xlabel=expected.get("xlabel", None))
+    figure, plot_configuration, mocks = plot_residuals_fixture
+    plt = mocks["plt"]
+    check_x_label(plt=plt, figure=figure, xlabel=plot_configuration.xlabel)
 
 
 def test_ylabel(plot_residuals_fixture):
-    mocks, expected = plot_residuals_fixture
-    plt, figure = mocks["plt"], mocks["figure"]
-    check_y_label(plt=plt, figure=figure, ylabel=expected.get("ylabel", None))
-
-
-def test_show_or_export(plot_residuals_fixture):
-    mocks, expected = plot_residuals_fixture
-    plt, figure = mocks["plt"], mocks["figure"]
-    check_show_or_export(
-        plt=plt, figure=figure, output_path=expected.get("output_path", None)
-    )
+    figure, plot_configuration, mocks = plot_residuals_fixture
+    plt = mocks["plt"]
+    check_y_label(plt=plt, figure=figure, ylabel=plot_configuration.ylabel)
 
 
 def test_grid(plot_residuals_fixture):
-    mocks, expected = plot_residuals_fixture
-    plt, figure = mocks["plt"], mocks["figure"]
-    check_grid(plt=plt, figure=figure, grid=expected.get("grid", False))
+    figure, plot_configuration, mocks = plot_residuals_fixture
+    plt = mocks["plt"]
+    check_grid(plt=plt, figure=figure, grid=plot_configuration.grid)
 
 
 def test_horizontal_line(plot_residuals_fixture):
-    mocks, _ = plot_residuals_fixture
-    plt, figure = mocks["plt"], mocks["figure"]
+    figure, plot_configuration, mocks = plot_residuals_fixture
+    plt = mocks["plt"]
     plt.hlines.assert_called_with(
         0, xmin=xmin, xmax=xmax, linestyles="dashed", figure=figure
     )
 
 
 def test_error_bar(plot_residuals_fixture):
-    mocks, _ = plot_residuals_fixture
+    figure, plot_configuration, mocks = plot_residuals_fixture
     plt, _ = mocks["plt"], mocks["figure"]
     check_error_bar(plt=plt, y=data.y - dummy_func(a, data.x))
 
 
 def test_plot(plot_residuals_fixture):
-    mocks, _ = plot_residuals_fixture
+    figure, plot_configuration, mocks = plot_residuals_fixture
     plt, _ = mocks["plt"], mocks["figure"]
     plt.plot.assert_not_called()
